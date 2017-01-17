d5891cf36410cf9df4a87e3c21c4a9883a242883
andrew.smith <smithan7@oregonstate.edu>
2017-01-17 11:26:31 -0800

    first commit

---
+++ src/Batch_testing.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Batch_testing.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	system("./../inference_coordination/Debug/inference_coordination");
+	return 0;
+}
